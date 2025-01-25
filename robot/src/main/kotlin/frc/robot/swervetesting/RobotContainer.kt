ed7de00dff683fe0675ffe29cdfd43defce731e9
Alameda Aztechs <frc4186@gmail.com>
2025-01-25 15:16:22 -0800

    Fixed robot Container's translating errors. Lots of remaining errors related to imports and other files

--- robot/src/main/kotlin/frc/robot/swervetesting/RobotContainer.kt
+++ robot/src/main/kotlin/frc/robot/swervetesting/RobotContainer.kt
@@ -1,4 +1,4 @@
-package frc.robot.swerve-testing
+package frc.robot.swervetesting
 //rishab y
 import java.util.List
 import edu.wpi.first.wpilibj.Joystick
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton
 import frc.robot.swervetesting.ModuleConstants.AutoConstants
 import frc.robot.swervetesting.ModuleConstants.DriveConstants
 import frc.robot.swervetesting.ModuleConstants.OIConstants
-import frc.robot.commands.SwerveJoystickCmd
+import frc.robot.swervetesting.SwerveJoystickCmd
 import frc.robot.swervetesting.SwerveSubsystem
 
 class RobotContainer {
@@ -37,7 +37,7 @@ class RobotContainer {
                 { driverJoytick.getRawAxis(OIConstants.kDriverXAxis) },
                 { driverJoytick.getRawAxis(OIConstants.kDriverRotAxis) },
                 { !driverJoytick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx) })
-        )
+        ) as Commmand // forcefully sets parameter to command
 
         configureButtonBindings()
     }
@@ -58,20 +58,20 @@ class RobotContainer {
 
             // 2. Generate trajectory
             val trajectory: Trajectory = TrajectoryGenerator.generateTrajectory(
-                Pose2d(0, 0, Rotation2d(0)),
-                List.of(
-                    Translation2d(1, 0),
-                    Translation2d(1, -1)
+                Pose2d(0.0, 0.0, Rotation2d(0.0)),
+                ListOf(
+                    Translation2d(1.0, 0.0),
+                    Translation2d(1.0, -1.0)
                 ),
-                Pose2d(2, -1, Rotation2d.fromDegrees(180)),
+                Pose2d(2.0, -1.0, Rotation2d.fromDegrees(180.0)),
                 trajectoryConfig
             )
 
             // 3. Define PID controllers for tracking trajectory
-            val xController: PIDController = PIDController(AutoConstants.kPXController, 0, 0)
-            val yController: PIDController = PIDController(AutoConstants.kPYController, 0, 0)
+            val xController: PIDController = PIDController(AutoConstants.kPXController, 0.0, 0.0)
+            val yController: PIDController = PIDController(AutoConstants.kPYController, 0.0, 0.0)
             val thetaController: ProfiledPIDController = ProfiledPIDController(
-                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints
+                AutoConstants.kPThetaController, 0.0, 0.0, AutoConstants.kThetaControllerConstraints
             )
             thetaController.enableContinuousInput(-Math.PI, Math.PI)
 
@@ -89,8 +89,9 @@ class RobotContainer {
 
             // 5. Add some init and wrap-up, and return everything
             return SequentialCommandGroup(
-                InstantCommand { swerveSubsystem.resetOdometry(trajectory.getInitialPose()) },
+                InstantCommand(Runnable { swerveSubsystem.resetOdometry(trajectory.initialPose) }),
                 swerveControllerCommand,
-                InstantCommand { swerveSubsystem.stopModules() })
+                InstantCommand(Runnable { swerveSubsystem.stopModules() })
+            )
         }
 }
