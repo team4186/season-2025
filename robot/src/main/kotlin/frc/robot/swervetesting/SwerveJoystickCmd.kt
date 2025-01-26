35025038d7a9e2417fbec464c220d4d53554823f
Alameda Aztechs <frc4186@gmail.com>
2025-01-25 16:05:47 -0800

    Fixed JoystickCmd. Lots of abstraction and changes due to commandBase being removed from frc. ngl dont really get it - Boyuan

--- robot/src/main/kotlin/frc/robot/swervetesting/SwerveJoystickCmd.kt
+++ robot/src/main/kotlin/frc/robot/swervetesting/SwerveJoystickCmd.kt
@@ -1,78 +1,80 @@
 package frc.robot.swervetesting
 
-import java.util.function.Supplier
-import edu.wpi.first.wpilibj.SlewRateLimiter
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds
-import edu.wpi.first.wpilibj.kinematics.SwerveModuleState
-import edu.wpi.first.wpilibj2.command.CommandBase
-import kotlin.math.*
+import edu.wpi.first.math.filter.SlewRateLimiter
+import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.math.kinematics.SwerveModuleState
+import edu.wpi.first.wpilibj2.command.Command
+import kotlin.math.abs
+
 // Change below when we move these files to their actual directories.
-import frc.robot.swervetesting.Constants.DriveConstants
-import frc.robot.swervetesting.Constants.OIConstants
+import frc.robot.swervetesting.ModuleConstants.DriveConstants
+import frc.robot.swervetesting.ModuleConstants.OIConstants
 import frc.robot.swervetesting.SwerveSubsystem
 
-class SwerveJoystickCmd: CommandBase() {
-    private val swerveSubsystem: SwerveSubsystem
-    private val xSpdFunction: () -> Double
-    private val ySpdFunction: () -> Double
-    private val turningSpdFunction: () -> Double
+class SwerveJoystickCmd(
+    private val swerveSubsystem: SwerveSubsystem, // Primary constructor parameters
+    private val xSpdFunction: () -> Double,
+    private val ySpdFunction: () -> Double,
+    private val turningSpdFunction: () -> Double,
     private val fieldOrientedFunction: () -> Boolean
-    private val xLimiter: SlewRateLimiter
-    private val yLimiter: SlewRateLimiter
-    private val turningLimiter: SlewRateLimiter
+) : Command() { // Initialize the superclass
 
-    constructor(swerveSubsystem: SwerveSubsystem, xSpdFunction: () -> Double, ySpdFunction: () -> Double, turningSpdFunction: () -> Double, fieldOrientedFunction: () -> Boolean) {
-        this.swerveSubsystem = swerveSubsystem
-        this.xSpdFunction = xSpdFunction
-        this.ySpdFunction = ySpdFunction
-        this.turningSpdFunction = turningSpdFunction
-        this.fieldOrientedFunction = fieldOrientedFunction
-        this.xLimiter = xLimiter
-        this.yLimiter = yLimiter
-        this.turningLimiter = SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond)
-        addRequirements(SwerveSubsystem)
+    // Slew rate limiters
+    private val xLimiter = SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond)
+    private val yLimiter = SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond)
+    private val turningLimiter = SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond)
+
+    init {
+        // Add subsystem requirements
+        addRequirements(swerveSubsystem)
     }
 
     override fun initialize() {
+        // Initialization logic (if needed)
     }
 
     override fun execute() {
-        // 1. get real time joystick inputs.
-        var xSpeed: Double = xSpdFunction()
-        var ySpeed: Double = ySpdFunction()
-        var turningSpeed: Double = turningSpdFunction()
+        // 1. Get real-time joystick inputs
+        var xSpeed = xSpdFunction()
+        var ySpeed = ySpdFunction()
+        var turningSpeed = turningSpdFunction()
 
-        // 2. apply deadband.
-        xSpeed =  if (abs(xSpeed) > OIConstants.kDeadband) xSpeed else 0.0
+        // 2. Apply deadband
+        xSpeed = if (abs(xSpeed) > OIConstants.kDeadband) xSpeed else 0.0
         ySpeed = if (abs(ySpeed) > OIConstants.kDeadband) ySpeed else 0.0
-        turningSpeed = if(abs(turningSpeed) > OIConstants.kDeadband) turningSpeed else 0.0
+        turningSpeed = if (abs(turningSpeed) > OIConstants.kDeadband) turningSpeed else 0.0
 
-        // 3. Make drive smoother.
+        // 3. Make drive smoother
         xSpeed = xLimiter.calculate(xSpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond
         ySpeed = yLimiter.calculate(ySpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond
         turningSpeed = turningLimiter.calculate(turningSpeed) * DriveConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond
 
-        // 4. Construct desired chassis speeds.
-
-        ChassisSpeeds chassisSpeeds
-        if(fieldOrientedFunction.get()) {
-            chassisSpeeds = chassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d())
+        // 4. Construct desired chassis speeds
+        val chassisSpeeds = if (fieldOrientedFunction()) {
+            ChassisSpeeds.fromFieldRelativeSpeeds(
+                xSpeed,
+                ySpeed,
+                turningSpeed,
+                swerveSubsystem.getRotation2d()
+            )
         } else {
             // Relative to the robot
-            chassisSpeeds = ChassisSpeeds(xSpeed, ySpeed, turningSpeed)
+            ChassisSpeeds(xSpeed, ySpeed, turningSpeed)
         }
 
-        // 5. Convert chassis speeds to individual module states.
+        // 5. Convert chassis speeds to individual module states
         val moduleStates: Array<SwerveModuleState> = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds)
 
+        // 6. Set module states
         swerveSubsystem.setModuleStates(moduleStates)
     }
 
-    override fun end(interrupted: boolean) {
+    override fun end(interrupted: Boolean) {
+        // Stop the drivetrain when the command ends
         swerveSubsystem.stopModules()
     }
 
-    override fun isFinished(): boolean {
-        return false
+    override fun isFinished(): Boolean {
+        return false // Command never finishes on its own
     }
 }
