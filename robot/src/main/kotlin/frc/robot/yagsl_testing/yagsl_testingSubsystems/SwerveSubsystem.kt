8a1a0dbc37b9fc99ced8d55f8034369ab6106b91
Aztechs <frc4186@gmail.com>
2025-02-02 15:21:42 -0800

    Add one comment

--- robot/src/main/kotlin/frc/robot/yagsl_testing/yagsl_testingSubsystems/SwerveSubsystem.kt
+++ robot/src/main/kotlin/frc/robot/yagsl_testing/yagsl_testingSubsystems/SwerveSubsystem.kt
@@ -55,12 +55,7 @@ import kotlin.math.pow
 
 class SwerveSubsystem : SubsystemBase {
     /**
-     * Gets the swerve drive object.
-     *
-     * @return [SwerveDrive]
-     */
-    /**
-     * Swerve drive object.
+     * Created using json config files in RobotContainer
      */
     val swerveDrive: SwerveDrive
 
@@ -162,56 +157,56 @@ class SwerveSubsystem : SubsystemBase {
     /**
      * Setup AutoBuilder for PathPlanner.
      */
-    fun setupPathPlanner() {
-        // Load the RobotConfig from the GUI settings. You should probably
-        // store this in your Constants file
-        val config: RobotConfig
-        try {
-            config = RobotConfig.fromGUISettings()
-
-            val enableFeedforward = true
-            // Configure AutoBuilder last
-            AutoBuilder.configure(
-                { this.pose },  // Robot pose supplier
-                { initialHolonomicPose: Pose2d? -> this.resetOdometry(initialHolonomicPose) },  // Method to reset odometry (will be called if your auto has a starting pose)
-                { this.robotVelocity },  // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-                { speedsRobotRelative, moduleFeedForwards ->
-                    if (enableFeedforward) {
-                        swerveDrive.drive(
-                            speedsRobotRelative,
-                            swerveDrive.kinematics.toSwerveModuleStates(speedsRobotRelative),
-                            moduleFeedForwards.linearForces()
-                        )
-                    } else {
-                        swerveDrive.setChassisSpeeds(speedsRobotRelative)
-                    }
-                },  // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
-                PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
-                    PIDConstants(5.0, 0.0, 0.0),  // Translation PID constants
-                    PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
-                ),
-                config,  // The robot configuration
-                {
-                    // Boolean supplier that controls when the path will be mirrored for the red alliance
-                    // This will flip the path being followed to the red side of the field.
-                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-                    val alliance = DriverStation.getAlliance()
-                    if (alliance.isPresent) {
-                        return@configure alliance.get() == Alliance.Red
-                    }
-                    false
-                },
-                this // Reference to this subsystem to set requirements
-            )
-        } catch (e: Exception) {
-            // Handle exception as needed
-            e.printStackTrace()
-        }
-
-        //Preload PathPlanner Path finding
-        // IF USING CUSTOM PATHFINDER ADD BEFORE THIS LINE
-        PathfindingCommand.warmupCommand().schedule()
-    }
+//    fun setupPathPlanner() {
+//        // Load the RobotConfig from the GUI settings. You should probably
+//        // store this in your Constants file
+//        val config: RobotConfig
+//        try {
+//            config = RobotConfig.fromGUISettings()
+//
+//            val enableFeedforward = true
+//            // Configure AutoBuilder last
+//            AutoBuilder.configure(
+//                { this.pose },  // Robot pose supplier
+//                { initialHolonomicPose: Pose2d? -> this.resetOdometry(initialHolonomicPose) },  // Method to reset odometry (will be called if your auto has a starting pose)
+//                { this.robotVelocity },  // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+//                { speedsRobotRelative, moduleFeedForwards ->
+//                    if (enableFeedforward) {
+//                        swerveDrive.drive(
+//                            speedsRobotRelative,
+//                            swerveDrive.kinematics.toSwerveModuleStates(speedsRobotRelative),
+//                            moduleFeedForwards.linearForces()
+//                        )
+//                    } else {
+//                        swerveDrive.setChassisSpeeds(speedsRobotRelative)
+//                    }
+//                },  // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+//                PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+//                    PIDConstants(5.0, 0.0, 0.0),  // Translation PID constants
+//                    PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+//                ),
+//                config,  // The robot configuration
+//                {
+//                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+//                    // This will flip the path being followed to the red side of the field.
+//                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+//                    val alliance = DriverStation.getAlliance()
+//                    if (alliance.isPresent) {
+//                        return@configure alliance.get() == Alliance.Red
+//                    }
+//                    false
+//                },
+//                this // Reference to this subsystem to set requirements
+//            )
+//        } catch (e: Exception) {
+//            // Handle exception as needed
+//            e.printStackTrace()
+//        }
+//
+//        //Preload PathPlanner Path finding
+//        // IF USING CUSTOM PATHFINDER ADD BEFORE THIS LINE
+//        PathfindingCommand.warmupCommand().schedule()
+//    }
 
     /**
      * Aim the robot at the target returned by PhotonVision.
