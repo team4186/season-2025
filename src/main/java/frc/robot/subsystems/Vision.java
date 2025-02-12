e74e98bcb87605e53fe71e784f60e93627a31ca0
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2025-02-12 13:32:23 -0800

    Improved directory structure

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.swervedrive;
+package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Microseconds;
 import static edu.wpi.first.units.Units.Milliseconds;
