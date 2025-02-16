5a5c1aa9096f0a83cee1254cb25b039fd4c75e94
Ziyao Yuan <zyuanboost2026@gmail.com>
2025-02-16 02:20:53 -0800

    Add convertMetric and ticksToDegrees methods to Units - Ziyao

--- src/main/java/frc/robot/Units.java
+++ src/main/java/frc/robot/Units.java
@@ -1,15 +1,51 @@
 package frc.robot;
 
-public class Units {
-    public static double FeetToMeters(double feet, double meters) {
-        meters = 0.3048 * feet;
-        return meters;
+public final class Units {
+    
+    private Units() {}
+
+    public static double FeetToMeters(double feet) {
+        return 0.3048 * feet;
     }
 
-    public static double TicksToMeters(double encoderTicks, String motorType) {
-        if (motorType == "NEO550") {
-            return encoderTicks *
+    public static double TicksToMeters(double encoderTicks, double wheelDiameter, String motorType) {
+    	if (motorType == "NEO550" || motorType == "NEOVORTEX" || motorType == "NEO") {
+        	return (42.0/wheelDiameter) * encoderTicks;
         }
-        //
     }
+    
+    public static double TicksToDegrees(double encoderTicks, String motorType) {
+	if (motorType == "NEO550" || motorType == "NEOVORTEX" || motorType == "NEO") {
+		return (encoderTicks/42.0) * 360;
+	}
+    }
+    
+    public static double InchesToCentimeters(double inches) {
+	return inches * 2.54;
+    }
+
+    public static double convertMetric(double value, MetricConversion fromMagnitude, MetricConversion toMagnitude) {
+	return value * toMagnitude.metricConversion/fromMagnitude.metricConversion;
+    }
+
+    public enum MetricConversion {
+	MILI(1000),
+	CENTI(100),
+	MICRO(1000000),
+	DEFAULT(1),
+	NANO(1000000000),
+	DECI(10),
+	KILO(1/1000),
+	GIGA(1/1000000000),
+	MEGA(1/1000000),
+	HECTA(1/100),
+	DEKA(1/10);
+
+	private final double metricConversion;
+
+	MetricConversion(conversion) {
+		this.metricConversion = conversion;
+	}	
+    }
+
 }
