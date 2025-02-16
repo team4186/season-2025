93c65ed9d82e30fd87843f4caa091ea9b7068bc2
Rishab <rishabry@gmail.com>
2025-02-15 19:00:07 -0800

    Clark- preleminary code for climber, values need to be added. Review later on. +added forward motion

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
-
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
 
 
 public class Climber {
@@ -19,7 +19,10 @@ public class Climber {
         motor = new SparkMax(worker, SparkLowLevel.MotorType.kBrushless);
         sensor = new DigitalInput(port);
     }
-    public void move(){
+    public Command forward (){
+        motor.setVoltage(-10);//place holder
+    }
+    public Command clamp(){
         while (sensor.get()){
           motor.set(sped);
         }
