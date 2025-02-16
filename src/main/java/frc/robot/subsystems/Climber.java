73b77defa3b26623f1635a0c2df7889cc895ea65
Rishab <rishabry@gmail.com>
2025-02-15 17:19:43 -0800

    Clark- preleminary code for climber, values need to be added. Review later on.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,4 +1,28 @@
 package frc.robot.subsystems;
 
+
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+
 public class Climber {
+    private SparkMax motor;
+    private DigitalInput sensor;
+    private static int port = 15; //place holder
+    private static int worker = 12; //place holder
+    private static int sped = 1; //place holder
+
+
+    public Climber(){
+        motor = new SparkMax(worker, SparkLowLevel.MotorType.kBrushless);
+        sensor = new DigitalInput(port);
+    }
+    public void move(){
+        while (sensor.get()){
+          motor.set(sped);
+        }
+        motor.stopMotor();
+    }
 }
