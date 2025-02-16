e69dd068462a32189c6946014aa05ed2823a4cdd
Aztechs <frc4186@gmail.com>
2025-02-15 17:09:42 -0800

    Add DigitalInput object for TFLuna
    Write a wrapper method for checking limit switch state.

--- src/main/java/frc/robot/subsystems/AlgaeProcessor.java
+++ src/main/java/frc/robot/subsystems/AlgaeProcessor.java
@@ -3,21 +3,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel;
-
-
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class AlgaeProcessor extends SubsystemBase {
-    // ReadyPosition in degrees
-    // 90 is placeholder, change when testing.
-    private static final double readyPos = 90;
+    // processorPos is the current position of the processor in degrees.
+    private final double processorPos;
     // Placeholder CanID (Not actually 1).
     private static final int CANIDSwing = 1;
     // Placeholder CAN
     private static final int CANIDIntake = 2;
 
-    private static final SparkMax angleMotor = new SparkMax(CANIDSwing, SparkLowLevel.MotorType.kBrushless);
+    // Change the input channel based on what is on the RoboRIO.
+    private final DigitalInput TFLuna =  new DigitalInput(0);
 
-    private static final SparkMax intakeMotor = new SparkMax(CANIDIntake, SparkLowLevel.MotorType.kBrushless);
+    private final SparkMax swingMotor = new SparkMax(CANIDSwing, SparkLowLevel.MotorType.kBrushless);
+
+    private final SparkMax intakeMotor = new SparkMax(CANIDIntake, SparkLowLevel.MotorType.kBrushless);
+
+    public AlgaeProcessor(double processorPos) {
+        this.processorPos = processorPos;
+    }
 
     public Command intakeAlgae() {
         // Should move to ready position and intake algae.
@@ -27,7 +32,17 @@ public class AlgaeProcessor extends SubsystemBase {
         // Should move the motors to launch algae.
     }
 
-    public Command moveToSafePos() {
-        // Should move the arm to inside frame perimeter.
+    public boolean algaeDetected() {
+        if (TFLuna.get()) {
+            return true;
+        } else if (!TFLuna.get()) {
+            return false;
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    public double getProcessorPos() {
+        return processorPos;
     }
 }
