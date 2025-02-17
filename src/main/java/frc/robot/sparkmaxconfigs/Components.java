31a5bb164b2b8e1732464fe05a6b28ffa37a0fd6
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2025-02-16 16:06:10 -0800

    Updated Components placeholders and removed unused variables

--- src/main/java/frc/robot/sparkmaxconfigs/Components.java
+++ src/main/java/frc/robot/sparkmaxconfigs/Components.java
@@ -6,11 +6,14 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 // Components Singleton
 public final class Components {
+
     private static Components instance = null;
+
     public SingleMotor algaeProcessorMotor = new AlgaeProcessorMotor().algaeMotor;
     public MotorSet elevatorMotors = new ElevatorMotors().elevatorMotors;
     public SingleMotor endEffectorMotor = new EndEffectorMotor().endEffectorMotor;
-    // private Constructor
+
+
     private Components() {}
 
 
@@ -29,23 +32,30 @@ public final class Components {
                 DefaultMotorConfigs.getInstance().DefaultConfig);
     }
 
-    public static class ClimberMotor { }
-    public static class DeAlgaeMotor { }
+
+    public static class ClimberMotor {
+        // TODO
+    }
+
+
+    public static class DeAlgaeMotor {
+        // TODO
+    }
+
+
     public static class ElevatorMotors {
-        public MotorSet elevatorMotors = new MotorSet(
-                new SparkMax(0, SparkLowLevel.MotorType.kBrushless), // change 0 and 1 for id's
-                new SparkMax(1, SparkLowLevel.MotorType.kBrushless),
+        public final MotorSet elevatorMotors = new MotorSet(
+                new SparkMax(0, SparkLowLevel.MotorType.kBrushless), // lead
+                new SparkMax(1, SparkLowLevel.MotorType.kBrushless), // follower
                 DefaultMotorConfigs.getInstance().DefaultConfig
         );
     }
 
+
     // TODO: change IDs
     public static final class EndEffectorMotor {
-        public final SparkMax motor;
-        public final SparkMaxConfig config;
-
-        public SingleMotor endEffectorMotor = new SingleMotor(
-                motor = new SparkMax(0, SparkLowLevel.MotorType.kBrushless),
-                config = DefaultMotorConfigs.getInstance().DefaultConfig);
+        public final SingleMotor endEffectorMotor = new SingleMotor(
+                new SparkMax(0, SparkLowLevel.MotorType.kBrushless),
+                DefaultMotorConfigs.getInstance().DefaultConfig);
     }
 }
