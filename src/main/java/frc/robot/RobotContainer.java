6e22ff64c4f39a30a1efba18d72e93059ae61289
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2025-02-22 13:44:31 -0800

    Updated motorComponents reference with Components class object reference update
    Signed-off-by: Jarek Rettinghouse <j.rettinghouse@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -61,8 +61,8 @@ public class RobotContainer {
                   Constants.ElevatorConstants.ELEVATOR_D));
 
   private final DeAlgae deAlgae = new DeAlgae(
-          motorComponents.deAlgaeWheelMotor,
-          motorComponents.deAlgaeAngleMotor,
+          motorComponents.deAlgaeMotors.wheelMotor,
+          motorComponents.deAlgaeMotors.angleMotor,
           new PIDController(
                   Constants.DeAlgaeConstants.DE_ALGAE_P,
                   Constants.DeAlgaeConstants.DE_ALGAE_P,
