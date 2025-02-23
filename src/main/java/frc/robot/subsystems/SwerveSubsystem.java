39a4f5081d1ed6a6adf808571732650ed80bbc82
Rishab <rishabry@gmail.com>
2025-02-22 16:49:07 -0800

    commented out sections that broke due to vision being deleted in SwerveSubsystem -Boyuan

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -34,7 +34,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import frc.robot.Constants;
-import frc.robot.subsystems.Vision.Cameras;
+//import frc.robot.subsystems.Vision.Cameras;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -58,7 +58,7 @@ public class SwerveSubsystem extends SubsystemBase {
   private final SwerveDrive swerveDrive;
   private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
   private final boolean visionDriveTest = false;
-  private Vision vision;
+  //private Vision vision;
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
@@ -94,7 +94,7 @@ public class SwerveSubsystem extends SubsystemBase {
 //    swerveDrive.pushOffsetsToEncoders(); // Set the absolute encoder to be used over the internal encoder and push the offsets onto it. Throws warning if not possible
 
     if (visionDriveTest) {
-      setupPhotonVision();
+      //setupPhotonVision();
       // Stop the odometry thread if we are using vision that way we can synchronize updates better.
       swerveDrive.stopOdometryThread();
     }
@@ -124,10 +124,10 @@ public class SwerveSubsystem extends SubsystemBase {
   /**
    * Set up the photon vision class.
    */
-  public void setupPhotonVision()
-  {
-    vision = new Vision(swerveDrive::getPose, swerveDrive.field);
-  }
+  //public void setupPhotonVision()
+//  {
+//    vision = new Vision(swerveDrive::getPose, swerveDrive.field);
+//  }
 
 
   @Override
@@ -135,7 +135,7 @@ public class SwerveSubsystem extends SubsystemBase {
     // When vision is enabled we must manually update odometry in SwerveDrive
     if (visionDriveTest) {
       swerveDrive.updateOdometry();
-      vision.updatePoseEstimation(swerveDrive);
+      //vision.updatePoseEstimation(swerveDrive);
     }
   }
 
@@ -205,21 +205,21 @@ public class SwerveSubsystem extends SubsystemBase {
    *
    * @return A {@link Command} which will run the alignment.
    */
-  public Command aimAtTarget(Cameras camera) {
-
-    return run(() -> {
-      Optional<PhotonPipelineResult> resultO = camera.getBestResult();
-      if (resultO.isPresent()) {
-        var result = resultO.get();
-        if (result.hasTargets()) {
-          drive(getTargetSpeeds(
-                  0,
-                  0,
-                  Rotation2d.fromDegrees(result.getBestTarget().getYaw()))); // Not sure if this will work, more math may be required.
-        }
-      }
-    });
-  }
+//  public Command aimAtTarget(Cameras camera) {
+//
+//    return run(() -> {
+//      Optional<PhotonPipelineResult> resultO = camera.getBestResult();
+//      if (resultO.isPresent()) {
+//        var result = resultO.get();
+//        if (result.hasTargets()) {
+//          drive(getTargetSpeeds(
+//                  0,
+//                  0,
+//                  Rotation2d.fromDegrees(result.getBestTarget().getYaw()))); // Not sure if this will work, more math may be required.
+//        }
+//      }
+//    });
+//  }
 
 
   /**
