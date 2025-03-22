d74673011a5fc04f1ee4745f970b15570e122507
plantainbread <zyuanboost2026@gmail.com>
2025-03-22 12:00:44 -0700

    Add MoveForwardAuto (untested)

---
+++ src/main/java/frc/robot/commands/actions/MoveForwardAuto.java
@@ -0,0 +1,44 @@
+package frc.robot.commands.actions;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class MoveForwardAuto extends Command {
+    /**
+     * Test this later in the pit. 
+     * To earn a ranking point, all enabled robots must pass its starting line and score a coral on the reef.
+     * Robot starting line is 7 ft, 4 in. Or 224 cm from the reef. 
+     */
+
+    private final SwerveSubsystem driveTrain;
+    private final Elevator elevatorSubsystem;
+    private boolean isFinished = false;
+    private final int level;
+    private final double driveDistance;
+
+    @Override
+    public MoveForwardAuto(SwerveSubsystem driveTrain, double driveDistance) {
+        this.driveTrain = driveTrain;
+        this.distance = distance;
+        addRequirements(SwerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        
+    }
+
+    @Override
+    public boolean isFinished() {
+        // placeholder
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
